a8d7774572106a34fa587c5dea7bd2fbc9787573
Winner <blessedwinner66@gmail.com>
2025-11-16 12:55:15 +0200

    Halfway through sidebar implementation

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -8,26 +8,26 @@ const Sidebar = () => {
             <p className="text-gray-500  text-xs">Productivity Central</p>
         </div>
         <div className="space-y-2 mt-4 border-t border-gray-300 p-1 lg:p-4">
-           <NavLink end={true} to='/admin' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 ${isActive && " bg-blue-500 text-white font-medium"}`} >
+           <NavLink end={true} to='/admin' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 font-medium ${isActive && " bg-blue-500 text-white"}`} >
             <LayoutDashboard className='min-w-4 w-5' />
             <p className='hidden md:inline-block'>Dashboard</p>
         </NavLink>
-             <NavLink to='/admin/chat' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 ${isActive && " bg-blue-500 text-white font-medium"}`} >
+             <NavLink to='/admin/chat' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 font-medium ${isActive && " bg-blue-500 text-white"}`} >
             <MessageSquare className='min-w-4 w-5' />
             <p className='hidden md:inline-block'>Chat</p>
         </NavLink>
 
-             <NavLink to='/admin/tasks' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 ${isActive && " bg-blue-500 text-white font-medium"}`} >
+             <NavLink to='/admin/tasks' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 font-medium ${isActive && " bg-blue-500 text-white"}`} >
             <SquareCheckBig className='min-w-4 w-5' />
             <p className='hidden md:inline-block'>Tasks</p>
         </NavLink>
          
-              <NavLink to='/admin/team' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 ${isActive && " bg-blue-500 text-white font-medium"}`} >
+              <NavLink to='/admin/team' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 font-medium ${isActive && " bg-blue-500 text-white"}`} >
             <Users className='min-w-4 w-5' />
             <p className='hidden md:inline-block'>Team</p>
         </NavLink>
 
-             <NavLink to='/admin/settings' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 ${isActive && " bg-blue-500 text-white font-medium"}`} >
+             <NavLink to='/admin/settings' className={({isActive})=>`flex py-3.5 px-3 md:px-9 items-center justify-center lg:justify-start rounded-xl text-sm gap-3 min-w-0 font-medium ${isActive && " bg-blue-500 text-white"}`} >
             <Settings className='min-w-4 w-5' />
             <p className='hidden md:inline-block'>Settings</p>
         </NavLink>
