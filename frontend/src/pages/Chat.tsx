b839d7729a558ba16224e73bad9cb33f826db3d7
Winner <blessedwinner66@gmail.com>
2025-11-18 10:00:54 +0200

    Sidebar feature for chat

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -8,20 +8,20 @@ const Chat = () => {
           <div className="bg-white rounded-xl p-5 max-w-60 w-60 space-y-4">
              <div className="flex justify-between items-center">
               <span className="flex gap-2 items-center">
-                <Users className="text-blue-400 w-4 h-4"/>
+                <Users className="text-blue-400 w-5 h-5"/>
                 <p className="font-semibold text-sm text-gray-800">Online Now</p>
               </span>
-              <p className="bg-gray-400 text-sm w-3 h-2 rounded-full">4</p>
+              <p className="bg-gray-300 text-gray-800 text-sm w-7 h-5.5 flex items-center justify-center rounded-full font-light">4</p>
               </div>
               <div className="space-y-6">
             { users.map((user,idx)=>(
-              <div key={idx}>
-                <div>
-                  <p>{user.charAt(0)}</p>
+              <div key={idx} className="flex gap-3 hover:bg-gray-200 px-2 py-3 rounded-xl cursor-pointer">
+                <div className="bg-blue-400 text-white w-10 h-10 flex items-center justify-center rounded-full">
+                  <p className="font-semibold">{user.charAt(0)}</p>
                 </div>
                 <div>
-                  <p>{user}</p>
-                  <p>Online</p>
+                  <p className="font-semibold text-gray-800 text-sm">{user}</p>
+                  <p className="text-xs text-gray-400">Online</p>
                 </div>
               </div>
             )) }
