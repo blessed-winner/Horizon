4e3987da8fb18528c950cd1f5479900c1c32dd04
Winner <blessedwinner66@gmail.com>
2025-11-18 20:47:46 +0200

    Full team overview component

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -3,8 +3,8 @@ import { userData } from "../assets/assets"
 
 const Team = () => {
   return (
-    <div className="flex-1 p-6 md:p-10">
-       <div className="space-y-8">
+    <div className="flex-1 p-6 md:p-10 space-y-6">
+       <div>
             <div className="flex justify-between">
           <div className="space-y-2">
             <h1 className="text-2xl font-semibold text-gray-800">Team Members</h1>
@@ -16,16 +16,18 @@ const Team = () => {
           </button>
         </div>
        </div>
-       <div>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           { userData.map((data,idx)=>(
-            <div key={idx}>
-              <div>{data.firstName.charAt(0) + data.lastName.charAt(0)}</div>
-              <div>
-                <h4>{data.firstName + " "+ data.lastName}</h4>
-                <span>{data.role}</span>
+            <div key={idx} className="bg-white px-6 py-4 rounded-xl space-y-8 shadow-xl hover:shadow-blue-200/70 transition">
+              <div className="flex gap-3">
+                 <div className="bg-blue-500 w-10 h-10 flex items-center justify-center rounded-full text-white font-medium">{data.firstName.charAt(0) + data.lastName.charAt(0)}</div>
+              <div className="space-y-1">
+                <h4 className="text-gray-800 text-sm font-semibold">{data.firstName + " "+ data.lastName}</h4>
+                <span className="text-xs bg-gray-200 px-2 py-0.5 rounded-full text-gray-600 font-medium">{data.role}</span>
               </div>
-              <button>
-                <Mail/>
+              </div>
+              <button className="group flex items-center gap-2 text-xs font-medium text-gray-800 justify-center bg-gray-100/80 border border-gray-400 w-full py-2 rounded-xl hover:bg-blue-400 transition cursor-pointer hover:text-white hover:border-none">
+                <Mail className="w-5 h-5 text-gray-800 group-hover:text-white"/>
                 <p>{data.email}</p>  
               </button>
             </div>
