23eb1d9e610079533a070d55ad9fdca8eefc4150
Winner <blessedwinner66@gmail.com>
2025-11-19 20:29:09 +0200

    Styled the profile settings section

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -4,30 +4,32 @@ import { notificationSettings } from "../assets/assets"
 const Settings = () => {
   return (
     <div className="flex-1 p-6 md:p-10">
-      <div>
-        <div>
-           <div>
-        <h2>Settings</h2>
-        <p>Manage your account and preferences</p>
+      <div className="space-y-8">
+        <div className="space-y-2">
+        <h2 className="font-bold text-3xl text-gray-800">Settings</h2>
+        <p className="text-sm text-light text-gray-500">Manage your account and preferences</p>
         </div>
-        <div>
-          <div>
-            <h3>Profile Settings</h3>
-            <p>Update your personal information</p>
+        <div className="bg-white px-8 py-12 rounded-xl shadow-lg">
+        <div className="space-y-3">
+          <div className="space-y-1">
+            <h3 className="font-semibold text-gray-800 text-2xl">Profile Settings</h3>
+            <p className="text-xs text-gray-500">Update your personal information</p>
           </div>
-          <div>
-            <label>Full Name</label>
-            <input type="text" placeholder="Enter your name" />
+          <div className="space-y-4">
+            <div className="flex flex-col  gap-2">
+            <label className="font-medium text-xs text-gray-800">Full Name</label>
+            <input type="text" placeholder="Enter your name" className="w-full border border-gray-300 px-6 py-3 bg-gray-100/80 placeholder:text-xs rounded-xl outline-none text-xs"/>
           </div>
-          <div>
-            <label>Email</label>
-            <input type="email" placeholder="your@email.com" />
+          <div className="flex flex-col gap-2">
+            <label className="text-xs font-medium text-gray-800">Email</label>
+            <input type="email" placeholder="your@email.com" className="w-full border border-gray-300 px-6 py-3 bg-gray-100/80 placeholder:text-xs rounded-xl outline-none text-xs" />
           </div>
-          <button>
-            <Save/>
+          <button className="flex items-center text-white font-medium text-sm gap-2 bg-gradient-to-r from-blue-600 to-blue-400 rounded-xl cursor-pointer p-3">
+            <Save className="w-4 h-4"/>
             <p>Save Changes</p>
-          </button>
-        </div>
+          </button> 
+          </div>
+          </div>
         </div>
         <div>
           <div>
